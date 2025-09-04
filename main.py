e49f03678b755af3cd76a48a8ba37b68492dc7fa
Stephen Hanson <spaehanson1@gmail.com>
2025-09-03 23:24:40 -0500

    finished bookbot

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
+
 from stats import count_words, count_unique_characters
 
 def main():
-    book_file_path = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file_path = sys.argv[1]
     book_text = get_book_text(book_file_path)
-    print(f"{count_words(book_text)} words found in the document")
-    print(count_unique_characters(book_text))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    for char in count_unique_characters(book_text):
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 
 def get_book_text(book_file_path):
     with open(book_file_path) as f:
--- stats.py
+++ stats.py
@@ -5,7 +5,20 @@ def count_words(text):
 def count_unique_characters(text):
     unique_characters = {}
     for char in text:
-        if char not in unique_characters:
-            unique_characters[char] = 0
-        unique_characters[char] += 1
-    return unique_characters
+        if char.isalpha() is False:
+            continue
+        lower_char = char.lower()
+        if lower_char not in unique_characters:
+            unique_characters[lower_char] = 0
+        unique_characters[lower_char] += 1
+    return sort_character_dictionary(unique_characters)
+
+def sort_character_dictionary(char_dict):
+    list_of_dicts = []
+    for key, value in char_dict.items():
+        list_of_dicts.append({"char": key, "num": value})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
+def sort_on(item):
+    return item["num"]
